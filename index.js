2b5ceaa33b2e8d2f6ec89560f2527f0addb1bf66
SilvioDoMine <arthurhenrique@live.com>
2022-02-27 18:31:49 -0300

    Remoção de semicolons

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ import path from 'path'
 
 const app = express()
 const port = 5000
-const directory = path.resolve();;
-
-console.log(path.join(directory, 'node_modules/three/examples/jsm'));
+const directory = path.resolve()
 
 app.use(express.static('public'));
-app.use('/build/', express.static(path.join(directory, 'node_modules/three/build')));
-app.use('/jsm/', express.static(path.join(directory, 'node_modules/three/examples/jsm')));
-app.use('/client', express.static(path.join(directory, 'src/client')));
+app.use('/build/', express.static(path.join(directory, 'node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(directory, 'node_modules/three/examples/jsm')))
+app.use('/client', express.static(path.join(directory, 'src/client')))
 
 app.listen(port, () => {
     console.log(`Server is running at localhost:${port}`)
